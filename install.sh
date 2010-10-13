bf79831034dfa9ab20075066dd542d83de8a021e
Matthew Hooker <mhooker@digg.com>
2010-10-12 20:26:11 -0400

    basic install script

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+if [ "`echo $0 | cut -c1`" = "/" ]; then
+    ZSHD_PATH=`dirname $0`
+else
+    ZSHD_PATH=$(dirname `pwd`/`echo $0 | cut -c3-`)
+fi
 
+if [ -L ~/.zshrc ]; then
+    unlink ~/.zshrc
+fi
+
+if [ -L ~/.zshd ]; then
+    unlink ~/.zshd
+fi
+
+ln -s ${ZSHD_PATH}/zshrc ${HOME}/.zshrc
+ln -s ${ZSHD_PATH} ${HOME}/.zsh.d
